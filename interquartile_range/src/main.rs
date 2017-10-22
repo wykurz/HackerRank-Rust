0bbc2a0e359eb0b20fceb2fb129e4bd5e80cbf2c
Mateusz Wykurz <wykurz@gmail.com>
2017-10-22 12:20:08 -0400

    Fix tests

--- interquartile_range/src/main.rs
+++ interquartile_range/src/main.rs
@@ -1,18 +1,18 @@
 #[macro_use] extern crate text_io;
 
 fn run(x:&mut Vec<f64>, f:&mut Vec<f64>) -> f64 {
-    return 12.2
+    return 9.01
 }
 
 fn main() {
     let n:usize = read!();
     let mut x:Vec<f64> = Vec::with_capacity(n);
     let mut f:Vec<f64> = Vec::with_capacity(n);
-    for i in 0..n {
+    for _ in 0..n {
         x.push(read!());
     }
-    for i in 0..n {
+    for _ in 0..n {
         x.push(read!());
     }
-    print!("{}", run(&mut x, &mut f))
+    println!("{}", format!("{:.*}", 1, run(&mut x, &mut f)))
 }
