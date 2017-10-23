0303147c91768cb6bd810cd5da68ed291434c11a
Mateusz Wykurz <wykurz@gmail.com>
2017-10-22 21:45:06 -0400

    Add missing test file

---
+++ interquartile_range/tests/integration.rs
@@ -0,0 +1,22 @@
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
+
+fn test_output(input:&str, output:&str) {
+    println!(concat!("target/debug/", env!("CARGO_PKG_NAME")));
+    let process = Command::new(concat!("target/debug/", env!("CARGO_PKG_NAME")))
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute process");
+
+    process.stdin.unwrap().write_all(input.as_bytes()).expect("Failed writing to stdin");
+
+    let mut res = String::new();
+    process.stdout.unwrap().read_to_string(&mut res).expect("Failed reading from to stdout");
+    assert_eq!(output, res);
+}
+
+#[test]
+fn test1() {
+    test_output("6\n6 12 8 10 20 16\n5 4 3 2 1 5", "9.0\n");
+}
